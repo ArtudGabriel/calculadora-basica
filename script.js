65b98d1ecff785da037151de51aed61c9de9b1a7
ArtudGabriel <gabriel_dutra98@hotmail.com>
2024-08-22 16:10:15 -0300

    Atualização funcional
    A calculadora começou a funcionar. Ainda possui alguns bugs e melhorias, mas já está funcional

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-var tela = document.getElementById('tela');
-let operador = document.querySelectorAll('.operador');
-let botoes = document.querySelectorAll('.botao');
+var n1 = '';
+var n2 = '';
+var operador = null;
 
-botoes.forEach(botao => {
-  botao.addEventListener("click", function(){
-    let valor = botao.getAttribute('data-valor');
-    tela.innerHTML = valor;
+document.querySelectorAll('.botao').forEach(botao => {
+  botao.addEventListener('click', () => {
+    const valor = botao.getAttribute('data-valor');
+
+    if (['+', '-', 'x', '/'].includes(valor)) {
+      operador = valor;
+      n2 = n1;
+      n1 = '';
+    }
+    else if (valor === '=') {
+      if (n2 && operador) {
+        n1 = calcular(n1, n2, operador);
+        operador = null;
+        n2 = '';
+      }
+    } else if (valor === 'C') {
+      n1 = '';
+      n2 = '';
+      operador = null;
+    }
+    else {
+      n1 += valor;
+    }
+    atualizarTela();
   });
 });
+
+function calcular(a, b, operador) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  switch (operador) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case 'x':
+      return a * b;
+    case '/':
+      return a / b;
+  }
+}
+
+function atualizarTela() {
+  document.getElementById('tela').innerText = n1 || operador || '0';
+}
