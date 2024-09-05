7a2b87dbead6d90c1698baf247fec93dd9ccd135
ArtudGabriel <gabriel_dutra98@hotmail.com>
2024-09-05 16:24:57 -0300

    Atualização visual e possibilidade de usar o teclado para operar a calculadora

--- script.js
+++ script.js
@@ -5,57 +5,86 @@ var operador = null;
 document.querySelectorAll('.botao').forEach(botao => {
   botao.addEventListener('click', () => {
     const valor = botao.getAttribute('data-valor');
-
-    if (['+', '-', 'x', '÷', '%'].includes(valor)) {
-      operador = valor;
-      n2 = n1;
-      n1 = '';
-    }
-    else if (valor === '=') {
-      if (n2 && operador) {
-        n1 = calcular(n1, n2, operador);
-        operador = null;
-        n2 = '';
-      }
-    } else if (valor === 'AC') {
-      n1 = '';
-      n2 = '';
-      operador = null;
-    } else if (valor === '√') {
-      operador = valor;
-      if (n1 && operador) {
-        n1 = calcular(n1, n2, operador);
-      }
-    } else {
-      n1 += valor;
-    }
-    atualizarTela();
+    processaInfo(valor);
   });
 });
 
-function calcular(n1, n2, operador) {
-  n1 = parseFloat(n1);
-  n2 = parseFloat(n2);
-  switch (operador) {
-    case '+':
-      return n2 + n1;
-    case '-':
-      return n2 - n1;
-    case 'x':
-      return n2 * n1;
-    case '÷':
-      return n2 / n1;
-    case '%':
-      return (n2 * n1) / 100;
-    case '√':
-      return Math.sqrt(n1);
+function processaInfo(valor) {
+  if (['+', '-', 'x', '÷', '%'].includes(valor)) {
+    operador = valor;
+    n2 = n1;
+    n1 = '';
   }
+  else if (valor === '=') {
+    if (n2 && operador) {
+      n1 = calcular(n1, n2, operador);
+      operador = null;
+      n2 = '';
+    }
+  } else if (valor === 'AC') {
+    n1 = '';
+    n2 = '';
+    operador = null;
+  } else if (valor === '√') {
+    operador = valor;
+    if (n1 && operador) {
+      n1 = calcular(n1, n2, operador);
+    }
+  } else {
+    n1 += valor;
+  }
+  atualizarTela();
 }
 
-function atualizarTela() {
-  if (n1 == Infinity) {
-    document.getElementById('tela').innerText = 'Error';
-  } else {
-    document.getElementById('tela').innerText = n1 || operador || '0';
+document.addEventListener('keydown', (teclas) => {
+  let tecla = teclas.key;
+  
+  if (!isNaN(tecla)) {
+    processaInfo(tecla);
+  } else if (tecla === '+') {
+    processaInfo('+');
+  } else if (tecla === '-') {
+    processaInfo('-');
+  } else if (tecla === '*') {
+    processaInfo('x');
+  } else if (tecla === '/') {
+    processaInfo('÷');
+  } else if (tecla === 'Enter') {
+    processaInfo('=');
+  } else if (tecla === 'Escape' || tecla === 'Backspace') {
+    processaInfo('AC');
+  } else if (tecla === '%') {
+    processaInfo('%');
+  } else if (tecla === 'r' || tecla === 'R') {
+    processaInfo('√');
+  } else if (tecla === '.' || tecla === ',') {
+    processaInfo('.');
   }
-}
+});
+
+  function calcular(n1, n2, operador) {
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
+    switch (operador) {
+      case '+':
+        return n2 + n1;
+      case '-':
+        return n2 - n1;
+      case 'x':
+        return n2 * n1;
+      case '÷':
+        return n2 / n1;
+      case '%':
+        return (n2 * n1) / 100;
+      case '√':
+        return Math.sqrt(n1);
+    }
+  }
+
+  function atualizarTela() {
+    if (n1 == Infinity) {
+      document.getElementById('tela').innerText = 'Error';
+    } else {
+      document.getElementById('tela').innerText = n1 || operador || '0';
+    }
+  }
